10c81e7da0b671aef41e8210d6655267750eb656
Azza434 <adwyer434@gmail.com>
2020-02-17 08:06:10 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'vehicle_log'
+app.config["MONGO_URI"] = "mongodb+srv://root:64XxzJZr7uXrfwyy@myfirstcluster-pffgj.mongodb.net/vehicle_log?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_data')
+def get_data():
+    return render_template("data.html", info=mongo.db.info.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
